7937e674df00cfb1f84670fa00f5f711e206983c
Kelton OBrien <kelton@keltono.net>
2025-07-09 15:20:51 -0500

    update

--- create_dotfiles.sh
+++ create_dotfiles.sh
@@ -7,11 +7,9 @@ mkdir -p ~/.config/nvim
 ln .vimrc ~/.config/nvim/init.vim
 ln .bashrc ~/.bashrc
 ln .bash_aliases ~/.bash_aliases
-mkdir -p ~/.i3/
-ln i3config ~/.i3/config
-ln .i3status.conf ~/.i3status.conf
-ln .XCompose ~/.XCompose
-mkdir -p ~/.config/nixpkgs/
-ln home.nix ~/.config/nixpkgs/home.nix
-mkdir -p ~/.config/alacritty
-ln alacritty.yml ~/.config/alacritty/alacritty.yml
+#mkdir -p ~/.i3/
+#ln i3config ~/.i3/config
+#ln .i3status.conf ~/.i3status.conf
+#ln .XCompose ~/.XCompose
+#mkdir -p ~/.config/nixpkgs/
+#ln home.nix ~/.config/nixpkgs/home.nix
